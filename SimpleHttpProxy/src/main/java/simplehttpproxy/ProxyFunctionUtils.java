e72a4a6e20ad9088c52bae41c58bf9cacf7b3aba
liangwenlin1234 <lwen1234@yahoo.com>
2019-03-01 12:46:11 -0500

    comment

---
+++ SimpleHttpProxy/src/main/java/simplehttpproxy/ProxyFunctionUtils.java
@@ -0,0 +1,83 @@
+package simplehttpproxy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class ProxyFunctionUtils {
+	private static Logger logger = Logger.getLogger(ProxyFunctionUtils.class);
+	
+	private ProxyFunctionUtils() {}
+    	
+	public static void inputToOutput(final InputStream input, final OutputStream output) throws IOException {
+		byte[] buff = new byte[2048];
+		int readLength = 0;
+
+		try {		
+			while ((readLength = input.read(buff)) != -1) {
+				output.write(buff, 0, readLength);
+			}
+			output.flush();
+
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+			
+			try {
+				output.close();
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+
+		}
+	}
+	
+	public static HttpURLConnection getHttpURLConnection(final HttpServletRequest request, final RequestMethod method) throws IOException {
+        URL urlToServer = null;
+        HttpURLConnection urlConnection = null;
+       
+        String requestUrl = request.getRequestURL().toString();
+        String queryString = request.getQueryString();
+        
+        // set URL. for GET, Query string is part of URL
+        requestUrl = requestUrl + (queryString == null? "" : "?" + queryString);
+
+		urlToServer = new URL(requestUrl);
+
+		urlConnection = (HttpURLConnection) urlToServer.openConnection();
+
+		urlConnection.setRequestMethod(method.name());
+		
+		// set request header
+		Enumeration<String> headerNames = request.getHeaderNames();
+		if (headerNames != null) {
+			while (headerNames.hasMoreElements()) {
+				String name = headerNames.nextElement();
+				String value = request.getHeader(name);
+				urlConnection.setRequestProperty(name, value);
+			}
+			
+		}
+		
+		logger.debug("proxy.url => " + urlConnection.getURL() + 
+			         "; Method=" + urlConnection.getRequestMethod() + 
+			         "; Input=" + urlConnection.getDoInput() + 
+			         "; Output=" + urlConnection.getDoInput());
+		
+		return urlConnection;
+	}
+    
+}
--- SimpleHttpProxy/src/main/java/simplehttpproxy/ProxyRestController.java
+++ SimpleHttpProxy/src/main/java/simplehttpproxy/ProxyRestController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/")
 public class ProxyRestController {
 	private static final Logger logger = Logger.getLogger(ProxyRestController.class);
+	
+	private final ProxyFunctionIntf proxyFunction;
+	
+	@Autowired
+	public ProxyRestController(ProxyFunctionIntf proxyFunction) {
+		this.proxyFunction = proxyFunction;
+	}
 
 	@RequestMapping(value="/**", method = RequestMethod.GET, produces=MediaType.ALL_VALUE)
 	@ResponseBody
     public void proxyGet(HttpServletRequest request, HttpServletResponse response) { 
 	    try {
-			ProxyFunction.sendGetAndGetResponse(request, response, RequestMethod.GET);
+	    	proxyFunction.sendAndGetResponse(request, response, RequestMethod.GET);
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
@@ -29,7 +37,7 @@ public class ProxyRestController {
 	@ResponseBody
     public void proxyPost(HttpServletRequest request, HttpServletResponse response) {
 	    try {
-			ProxyFunction.sendPostAndGetResponse(request, response, RequestMethod.POST);
+	    	proxyFunction.sendAndGetResponse(request, response, RequestMethod.POST);
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
